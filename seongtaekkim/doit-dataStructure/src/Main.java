7ff5a2a8760abbf704d5288491b42e783877433f
seongtaekkim <chxortnl@gmail.com>
2022-01-18 06:30:26 +0900

    03-2 lenear search sentinel search

--- seongtaekkim/doit-dataStructure/src/Main.java
+++ seongtaekkim/doit-dataStructure/src/Main.java
@@ -3,10 +3,13 @@ public class Main {
 
     /**
      * 1th : Basic_algorithms
+     * 2th : Basic_datastructure
+     * 3th : Search
      * @param args
      */
     public static void main(String[] args) {
         //new Basic_algorithms();
-        new Basic_datastructure();
+        //new Basic_datastructure();
+        new Search();
     }
 }
