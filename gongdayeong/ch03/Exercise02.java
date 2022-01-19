3377058a02fe7aee3950dcf40197f4181d35b0e0
dayoung <dayoung@danawa.com>
2022-01-19 23:21:02 +0900

    20220119 선형검색

---
+++ gongdayeong/ch03/Exercise02.java
@@ -0,0 +1,84 @@
+package ch03;
+
+import java.util.Scanner;
+
+public class Exercise02 {
+
+	static int SeqSearchSen(int[] a, int n, int key) {
+		int i = 0;
+		
+		a[n] = key;
+		
+		System.out.printf("   |");
+		for(int j=0; j<n; j++)
+			System.out.printf("%3d",j);
+		System.out.println();
+		
+		System.out.println("---+----------------------");
+		
+//		System.out.print("---+");
+//		for (int k = 0; k < 4 * n + 2; k++)
+//			System.out.print("-");
+//		System.out.println();
+		
+		while(i < n) {
+			System.out.printf("   |");
+			for(int j=0;j<=n;j++) {
+				if(j == i)
+					System.out.printf("%3s","*");
+				else
+					System.out.printf("%3s","");
+			}
+			
+			System.out.println();
+			
+			System.out.printf("  "+i+"|");
+			for(int k=0; k<n; k++)
+				System.out.printf("%3d",a[k]);
+			System.out.println();
+			
+			if(a[i] == key)
+				break;
+			
+			i++;
+		}
+		
+		return i == n ? -1 : i;
+		
+//		for (int i = 0; i < n; i++) {
+//			System.out.print("   |");
+//			System.out.printf(String.format("%%%ds*\n", (i * 4) + 3), "");
+//			System.out.printf("%3d|", i);
+//			for (int k = 0; k < n; k++)
+//				System.out.printf("%4d", a[k]);
+//			System.out.println("\n   |");
+//			if (a[i] == key)
+//				return i;
+//		}
+//		return -1;
+	}
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("요솟수:");
+		int num = scan.nextInt();
+		int[] x = new int[num+1];
+		
+		for(int i=0; i<num; i++) {
+			System.out.println("x["+i+"]:");
+			x[i] = scan.nextInt();
+		}
+		
+		System.out.println("검색할값:");
+		int ky = scan.nextInt();
+		
+		int idx = SeqSearchSen(x, num, ky);
+		
+		if (idx == -1) 
+			System.out.println("그 값의 요소가 없습니다.");
+		else
+			System.out.println(ky+"은(는) x["+idx+"]에 있습니다.");
+	}
+
+}
