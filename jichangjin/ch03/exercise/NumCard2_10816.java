eccd26f6d127b8af0edc42f3ec293f2f26626b03
jchj108 <jchj108@naver.com>
2022-02-02 23:27:11 +0900

    Q7 Comparator 시력 찾기

--- jichangjin/ch03/exercise/NumCard2_10816.java
+++ jichangjin/ch03/exercise/NumCard2_10816.java
@@ -1,8 +1,10 @@
 package jichangjin.ch03.exercise;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
@@ -10,11 +12,11 @@ public class NumCard2_10816 {
 
 	static int N, M;
 	static int[] card;
-	static int[] num;
-	static int[] result;
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
 		N = Integer.parseInt(br.readLine());
 		card = new int[N];
 		
@@ -23,54 +25,50 @@ public class NumCard2_10816 {
 			card[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		M = Integer.parseInt(br.readLine());
-		num = new int[M];
-		st = new StringTokenizer(br.readLine());
-		result = new int[M]; // 결과를 담을 배열
-		
 		Arrays.sort(card);
+
+		M = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		
 		
 		for(int i = 0; i < M; i++) {
-			num[i] = Integer.parseInt(st.nextToken());
+			int key = Integer.parseInt(st.nextToken());
+			bw.write((upperBound(key) - lowerBound(key) + " "));
 		}
-		for(int i = 0; i < M; i++) {
-			result[i] = binarySearch(num[i]);
-			System.out.print(result[i] + " ");
-		}
+		bw.flush();
+		bw.close();
 	}
 
-	private static int binarySearch(int num) {
+	private static int lowerBound(int key) {
 		
-		int pl = 0;
-		int pr = card.length-1;
-		int pc = 0;
+		int lo = 0;
+		int hi = card.length;
 		
-		while(pl < pr) {
-			pc = (pl + pr) / 2;
-			if(card[pc] == num) {
-				int lower = pc;
-				int upper = pc;
-				while(lower > 0) {
-					if(card[lower - 1] == num) {
-						lower--;
-					} else {
-						break;
-					}
-				}
-				while(upper < card.length - 1) {
-					if(card[upper + 1] == num) {
-						upper++;
-					} else {
-						break;
-					}
-				}
-				return upper - lower + 1;
-			} else if (card[pc] > num) {
-				pr = pc - 1;
+		while(lo < hi) {
+			int mid = (lo + hi) / 2;
+			
+			if(key <= card[mid]) {
+				hi = mid;
 			} else {
-				pl = pc + 1;
+				lo = mid + 1;
 			}
-		} 
-		return 0;
+		}
+		return lo;
+	}
+
+	private static int upperBound(int key) {
+		int lo = 0;
+		int hi = card.length;
+		
+		while(lo < hi) {
+			int mid = (lo + hi) / 2;
+			
+			if(key < card[mid]) {
+				hi = mid;
+			} else {
+				lo = mid + 1;
+			}
+		}
+		return lo;
 	}
 }
