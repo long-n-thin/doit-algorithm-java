34fea9ed7dfb3cd23158a78aa7f498eb16ffc2aa
inhalin <inhalin.kim@gmail.com>
2022-02-09 01:43:17 +0900

    Solve q4 - linear queue implementation

---
+++ kimin/src/ch04/IntArrayQueue.java
@@ -0,0 +1,93 @@
+package ch04;
+
+import java.util.Arrays;
+
+public class IntArrayQueue {
+    private int max;
+    private int num;
+    private int[] q;
+
+    public static class QueueOverflowException extends RuntimeException {
+        public QueueOverflowException() {
+            System.out.println("Queue is full.");
+        }
+    }
+
+    public static class QueueEmptyException extends RuntimeException {
+        public QueueEmptyException() {
+            System.out.println("Queue is empty.");
+        }
+    }
+
+    public IntArrayQueue(int size) {
+        this.max = size;
+        this.num = 0;
+        this.q = new int[size];
+    }
+
+    public boolean isEmpty() {
+        return num == 0;
+    }
+
+    public boolean isFull() {
+        return num == max;
+    }
+
+    public void enqueue(int value) {
+        if (isFull()) {
+            throw new QueueOverflowException();
+        }
+
+        q[num++] = value;
+    }
+
+    public int dequeue() {
+        if (isEmpty()) {
+            throw new QueueEmptyException();
+        }
+
+        int front = q[0];
+
+        if (num > 0) {
+            for (int i = 0; i < num - 1; i++) {
+                q[i] = q[i + 1];
+            }
+        }
+        num--;
+
+        return front;
+    }
+
+    public int peek() {
+        if (num == 0) {
+            throw new QueueEmptyException();
+        }
+
+        return q[0];
+    }
+
+    public void clear() {
+        Arrays.fill(q, 0);
+        num = 0;
+    }
+
+    public int capacity() {
+        return max;
+    }
+
+    public StringBuffer dump() {
+        StringBuffer str = new StringBuffer();
+
+        str.append("[");
+        for (int i = 0; i < num; i++) {
+            str.append(q[i]);
+
+            if (i < num - 1) {
+                str.append(", ");
+            }
+        }
+        str.append("]");
+
+        return str;
+    }
+}
--- kimin/src/ch04/Main.java
+++ kimin/src/ch04/Main.java
@@ -1,5 +1,7 @@
 package ch04;
 
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
 //        IntStack stack = new IntStack(4);
@@ -20,30 +22,46 @@ public class Main {
 //        System.out.println(stack.isEmpty());
 //        stack.dump();
 
-        LinkedStack stack = new LinkedStack();
-        System.out.println("1 isEmpty: " + stack.isEmpty());
-        stack.push(1);
-        System.out.println("2 isEmpty: " + stack.isEmpty());
-        stack.push(2);
-        stack.push(3);
-        System.out.println("3 peek: " + stack.peek());
-        System.out.println("4 peek: " + stack.peek());
-        int popped = stack.pop();
-        System.out.println("5 popped: " + popped);
-        System.out.println("6 peek: " + stack.peek());
-        int popped1 = stack.pop();
-        System.out.println("7 popped: " + popped1);
-        stack.pop();
-        int popped2 = stack.pop();
-        System.out.println("8 popped: " + popped2);
-        System.out.println("9 isEmpty: " + stack.isEmpty());
-        System.out.println("10 peek: " + stack.peek());
-        stack.push(4);
-        System.out.println("11 isEmpty: " + stack.isEmpty());
-        System.out.println("12 peek: " + stack.peek());
-        stack.delete();
-        System.out.println("13 isEmpty: " + stack.isEmpty());
-        System.out.println("14 peek: " + stack.peek());
+//        LinkedStack stack = new LinkedStack();
+//        System.out.println("1 isEmpty: " + stack.isEmpty());
+//        stack.push(1);
+//        System.out.println("2 isEmpty: " + stack.isEmpty());
+//        stack.push(2);
+//        stack.push(3);
+//        System.out.println("3 peek: " + stack.peek());
+//        System.out.println("4 peek: " + stack.peek());
+//        int popped = stack.pop();
+//        System.out.println("5 popped: " + popped);
+//        System.out.println("6 peek: " + stack.peek());
+//        int popped1 = stack.pop();
+//        System.out.println("7 popped: " + popped1);
+//        stack.pop();
+//        int popped2 = stack.pop();
+//        System.out.println("8 popped: " + popped2);
+//        System.out.println("9 isEmpty: " + stack.isEmpty());
+//        System.out.println("10 peek: " + stack.peek());
+//        stack.push(4);
+//        System.out.println("11 isEmpty: " + stack.isEmpty());
+//        System.out.println("12 peek: " + stack.peek());
+//        stack.delete();
+//        System.out.println("13 isEmpty: " + stack.isEmpty());
+//        System.out.println("14 peek: " + stack.peek());
 
+        // TODO: Q4 선형 큐(p.146)
+        IntArrayQueue arrq = new IntArrayQueue(4);
+        System.out.println("capacity: " + arrq.capacity());
+        System.out.println("isEmpty: " + arrq.isEmpty());
+        arrq.enqueue(1);
+        int dq = arrq.dequeue();
+        System.out.println("dequeue: " + dq);
+        arrq.enqueue(2);
+        arrq.enqueue(3);
+        arrq.enqueue(4);
+        System.out.println("isFull: " + arrq.isFull());
+        System.out.println("peek: " + arrq.peek());
+        System.out.println("dump: " + arrq.dump());
+        arrq.clear();
+        System.out.println("dump: " + arrq.dump());
+        arrq.dequeue();
     }
 }
