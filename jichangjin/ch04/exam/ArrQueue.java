1e757708f9b661a2e52daa3d0cc09455ece6ddf8
kim <kim@DESKTOP-9JKNRLV>
2022-02-11 13:27:41 +0900

    카드2

---
+++ jichangjin/ch04/exam/ArrQueue.java
@@ -0,0 +1,8 @@
+package jichangjin.ch04.exam;
+
+public class ArrQueue {
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ jichangjin/ch04/practice/Card2_2164.java
@@ -0,0 +1,30 @@
+package jichangjin.ch04.practice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Card2_2164 {
+
+	static int N;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		
+		Queue<Integer> q = new LinkedList<Integer>();
+		
+		for(int i = 1; i < N+1; i++) {
+			q.add(i);
+		}
+		
+		while(q.size() > 1) {
+			q.remove();
+			q.add(q.poll());
+		}
+		System.out.println(q.poll());
+	}
+}
