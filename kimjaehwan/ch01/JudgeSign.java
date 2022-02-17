4a0ebc2ec14bc9273af718dcf204169e12cc3ad0
jaehwaii <ypmc91@gmail.com>
2022-02-17 18:06:48 +0900

    judgeSign 실습 구현

---
+++ kimjaehwan/ch01/JudgeSign.java
@@ -0,0 +1,19 @@
+package kimjaehwan.ch01;
+
+import java.util.Scanner;
+
+public class JudgeSign {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println("정수를 입력하세요 : ");
+        int n = stdIn.nextInt();
+
+        if(n > 0)
+            System.out.println("이 수는 양수");
+        else if (n < 0)
+            System.out.println("이 수는 음수");
+        else
+            System.out.println("이 수는 0");
+    }
+}
